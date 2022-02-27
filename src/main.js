88f05c99ccfbe3287fbc3fc8e8232cc00bc98f81
JaquelineRocio <jaquelineramosvargas@gmail.com>
2022-02-27 17:35:37 -0500

    Mostrando los posters y las descripciones

--- src/main.js
+++ src/main.js
@@ -1,6 +1,38 @@
-import { example } from './data.js';
+import {showMovies } from './data.js';
 // import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
+import data from './data/ghibli/ghibli.js';
 // import data from './data/rickandmorty/rickandmorty.js';
+let i=0;
+console.log(data.films);
 
-console.log(example, data);
+while(i<data.films.length)
+{
+const targetBox=document.createElement('div');
+const target = document.createElement('div');
+const faceTarget = document.createElement('div');
+const poster = document.createElement('img');
+const backTarget= document.createElement('div');
+
+poster.src = showMovies(data.films[i].poster);
+
+backTarget.textContent = showMovies(data.films[i].description);
+
+targetBox.classList.add('target-box');
+target.classList.add('target');
+faceTarget.classList.add('faceTarget');
+backTarget.classList.add('backTarget');
+poster.classList.add('poster');
+
+document.getElementById('root').appendChild(targetBox);
+targetBox.insertAdjacentElement('afterbegin',target);
+
+target.insertAdjacentElement('afterbegin',backTarget);
+target.insertAdjacentElement('afterbegin',faceTarget);
+
+faceTarget.insertAdjacentElement('afterbegin',poster);
+
+
+
+i++;
+}
+
