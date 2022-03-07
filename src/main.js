35424ab61b35100b2a378dd1dc0865348761be5d
Lisette Frank <lisettefrank97@gmail.com>
2022-03-07 12:32:05 -0500

    Función score y menu desplagable

--- src/main.js
+++ src/main.js
@@ -47,23 +47,45 @@ function showFilms(films){
     }
     return box;
 }
-let container=showFilms(data.films);
 
-const sortTitle = document.getElementById('alfabeticamente');
+let container=showFilms(data.films);
 let cont = 0;
 
-sortTitle.addEventListener('click',()=>{
-    
-    let sortFilms = data.films;
-    if(cont==0)
-    {
-        sortFilms=sortFilms.sort((a, b) => (a.title > b.title) ? 1 : -1);
-        showFilms(sortFilms);
-        container.classList.add('invisible');
-    }
-    cont++;
 
+function sort() {
+    let sortFilms = data.films;
+    sortFilms=sortFilms.sort((a, b) => (a.title > b.title) ? 1 : -1);
+    let allSortFilms= showFilms(sortFilms);
+    container.classList.add('invisible');
+    sortTitle.removeEventListener('click', sort)
+}
+const sortTitle = document.getElementById('moviesChoose');
+sortTitle.addEventListener('click', sort);
+
+
+function score() {
+    
+    let scoreFilms= data.films;
+    scoreFilms= scoreFilms.sort((a,b) => (parseInt(a.rt_score) < parseInt(b.rt_score)) ? 1 : -1);
+    let allScoreFilms= showFilms(scoreFilms);
+    container.classList.add('invisible');
+    scorMovie.removeEventListener('click', score);
+}
+const scorMovie =document.getElementById('movieScor');
+scorMovie.addEventListener('click', score);
+
+ 
+let start= document.getElementById('buttonO');
+start.addEventListener("click", ()=>{
+
+    let seeOpcionsF = document.querySelector('.button').style.display;
+    if (seeOpcionsF == "none"){
+       document.querySelector('.button').style.display = "block";
+    }
 })
+
+
+
 
 
 
