fb504aa6e7dd88a09745690566af8bc477bf11dc
Lisette Frank <lisettefrank97@gmail.com>
2022-03-08 19:39:19 -0500

    Menu de opciones desplegable

--- src/data.js
+++ src/data.js
@@ -11,3 +11,5 @@ export const sortAZFilms = (data) => {
 export const anotherExample = () => {
   return 'OMG';
 };
+
+
--- src/main.js
+++ src/main.js
@@ -57,15 +57,14 @@ let cont = 0;
 
 
 function sort() {
-    let sortFilms = data.films;
-    sortFilms=sortFilms.sort((a, b) => (a.title > b.title) ? 1 : -1);
-    let allSortFilms= showFilms(sortFilms);
+    let films = sortAZFilms(data,container);
+    films= films.sort((a, b) => (a.title > b.title) ? 1 : -1);
+    showFilms(films);
     container.classList.add('invisible');
     sortTitle.removeEventListener('click', sort)
 }
 const sortTitle = document.getElementById('moviesChoose');
 sortTitle.addEventListener('click', sort);
-
 
 function score() {
     
@@ -75,27 +74,7 @@ function score() {
     container.classList.add('invisible');
     scorMovie.removeEventListener('click', score);
 }
+
 const scorMovie =document.getElementById('movieScor');
 scorMovie.addEventListener('click', score);
 
- 
-let start= document.getElementById('buttonO');
-start.addEventListener("click", ()=>{
-
-    let seeOpcionsF = document.querySelector('.button').style.display;
-    if (seeOpcionsF == "none"){
-       document.querySelector('.button').style.display = "block";
-    }
-})
-
-
-/*const sortTitle = document.getElementById('alfabeticamente');
-sortTitle.addEventListener('click',()=>{
-    if(cont==0)
-    {
-        container.style.display='none';
-        let Films = sortAZFilms(data,container);
-        showFilms(Films);
-    }
-    cont++;
-})*/
